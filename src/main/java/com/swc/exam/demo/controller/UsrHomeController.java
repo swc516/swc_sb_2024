0ccd7baadf1a5ac672b7c288a88bcf3e4bc04ecd
swc516 <swc516s@gmail.com>
2024-03-05 17:22:38 +0900

    호출 시 반환되는 값이 1씩 커지는 컨트롤러 액션 구현

--- src/main/java/com/swc/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/swc/exam/demo/controller/UsrHomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	private int count;
+	
+	public UsrHomeController() {
+		count = 0;
+	}
 	@RequestMapping("usr/home/main")
 	@ResponseBody
 	public String showMain() {
@@ -21,6 +26,12 @@ public class UsrHomeController {
 	@RequestMapping("usr/home/main3")
 	@ResponseBody
 	public String showMain3() {
-		return "또 봐요";
+		return "또 만나요";
+	}
+	
+	@RequestMapping("usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		return count++;
 	}
 }
