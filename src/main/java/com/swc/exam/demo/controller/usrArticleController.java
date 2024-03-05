eb9da8dda609039520316a976cd122679826aa64
swc516 <swc516s@gmail.com>
2024-03-05 18:20:22 +0900

    테스트용 게시물 10개 자동생성 기능 구현, 삭제기능 구현 중

--- src/main/java/com/swc/exam/demo/controller/usrArticleController.java
+++ src/main/java/com/swc/exam/demo/controller/usrArticleController.java
@@ -1,6 +1,5 @@
 package com.swc.exam.demo.controller;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,34 +9,53 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.swc.exam.vo.Article;
 
-
 @Controller
 public class usrArticleController {
 	private int articlesLastId;
 	private List<Article> articles;
-	
+
 	public usrArticleController() {
-		articlesLastId = 0 ;
+		articlesLastId = 0;
 		articles = new ArrayList<>();
+
+		makeTestData();
 	}
-	
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = articlesLastId + 1;
+			String title = "제목"+ i;
+			String body = "내용"+ i;
+			Article article = new Article(id, title, body);
+
+			articles.add(article);
+			articlesLastId = id;
+		}
+	}
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = articlesLastId + 1;;
+		int id = articlesLastId + 1;
 		Article article = new Article(id, title, body);
 		articles.add(article);
 		articlesLastId = id;
 
 		return article;
 	}
-	
-	
+
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articles;
 	}
-	
-}
 
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		articles.remove(id - 1);
+
+		return id + "번글이 삭제되었습니다.";
+	}
+
+}
