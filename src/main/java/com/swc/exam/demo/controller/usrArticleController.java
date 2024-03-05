c55dc15751995ad672168c6c3da7b8a63d6f1228
swc516 <swc516s@gmail.com>
2024-03-05 19:48:01 +0900

    게시물 수정기능

--- src/main/java/com/swc/exam/demo/controller/usrArticleController.java
+++ src/main/java/com/swc/exam/demo/controller/usrArticleController.java
@@ -59,7 +59,12 @@ public class usrArticleController {
 		articles.remove(article);
 	}
 
-	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);;
+		article.setBody(body);
+	}
 	// 서비스 메서드 끝
 
 	
@@ -90,6 +95,21 @@ public class usrArticleController {
 		deleteArticle(id);
 		return id + "번 게시물을 삭제하였습니다.";
 	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id, title, body);
+		return id + "번 게시물을 수정하였습니다.";
+	}
+
+
 
 	//액션 메서드 끝 
 
