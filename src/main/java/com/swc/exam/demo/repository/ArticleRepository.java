d97009aca6de6dab20b8c1406b0296021ebb5185
swc516 <swc516s@gmail.com>
2024-03-05 22:54:19 +0900

    DELETE, UPDATE, SELECT 관련 쿼리를 메서드와 연결

--- src/main/java/com/swc/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/swc/exam/demo/repository/ArticleRepository.java
@@ -3,9 +3,11 @@ package com.swc.exam.demo.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Component;
 
 import com.swc.exam.vo.Article;
@@ -17,9 +19,12 @@ public interface ArticleRepository {
 	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
-	public void deleteArticle(int id);
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(@Param("id") int id);
 
-	public void modifyArticle(int id, String title, String body);
+	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
+	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 	
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 }
