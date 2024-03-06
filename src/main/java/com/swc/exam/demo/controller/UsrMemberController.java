f9e487a127065a500e597b610a509263806c877d
swc516 <swc516s@gmail.com>
2024-03-06 14:27:28 +0900

    회원가입 시, 입력데이터 유효성 체크

--- src/main/java/com/swc/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/swc/exam/demo/controller/UsrMemberController.java
@@ -23,14 +23,39 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
-		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		
+		if ( loginId == null || loginId.trim().length() == 0 ) { // 공백제거한 길이가 0
+			return "loginId(을)를 입력해주세요.";
+		}
+		
+		if ( loginPw == null || loginPw.trim().length() == 0 ) {
+			return "loginPw(을)를 입력해주세요.";
+		}
+		
+		if ( name == null || name.trim().length() == 0 ) {
+			return "name(을)를 입력해주세요.";
+		}
+		
+		if ( nickname == null || nickname.trim().length() == 0 ) {
+			return "nickname(을)를 입력해주세요.";
+		}
+		
+		if ( cellphoneNo == null || cellphoneNo.trim().length() == 0 ) {
+			return "cellphoneNo(을)를 입력해주세요.";
+		}
+		
+		if ( email == null || email.trim().length() == 0 ) {
+			return "email(을)를 입력해주세요.";
+		}
+		
+		
+		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		if ( id == -1 ) {
 			return "해당 로그인아이디는 이미 사용중입니다.";
 		}
 		
 		Member member = memberService.getMemberById(id);
-		
+
 		return member;
 	}
 
