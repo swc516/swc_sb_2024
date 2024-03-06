d945ffce47ab3e4ff9157543a5d668700cc254fd
swc516 <swc516s@gmail.com>
2024-03-06 13:57:10 +0900

    MemberRepository추가, 회원가입 처리

--- src/main/java/com/swc/exam/demo/repository/MemberRepository.java
+++ src/main/java/com/swc/exam/demo/repository/MemberRepository.java
@@ -1,5 +1,26 @@
 package com.swc.exam.demo.repository;
 
-public class MemberRepository {
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberRepository {
+
+	
+	@Insert("""
+			INSERT INTO `member`
+			SET regDate = NOW(),
+			updateDate = NOW(),
+			loginId = #{loginId},
+			loginPw = #{loginPw},
+			`name` = #{name},
+			nickname = #{nickname},
+			cellphoneNo = #{cellphoneNo},
+			email = #{email}
+						""")
+	void join(@Param("loginId")String loginId, @Param("loginPw")String loginPw, @Param("name")String name, @Param("nickname")String nickname, @Param("cellphoneNo")String cellphoneNo, @Param("email")String email);
+	
+	
 
 }
--- src/main/java/com/swc/exam/demo/service/MemberService.java
+++ src/main/java/com/swc/exam/demo/service/MemberService.java
@@ -7,10 +7,13 @@ import com.swc.exam.demo.repository.MemberRepository;
 @Service
 public class MemberService {
 
-	//private MemberRepository memberRepository;
+	private MemberRepository memberRepository;
 
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-//		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 	}
 
 }
