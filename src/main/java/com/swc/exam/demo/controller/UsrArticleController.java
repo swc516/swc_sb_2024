ec6e49f96a688b1b0b66127aafffe701a33c1d84
swc0516s <swc0516@naver.com>
2024-03-08 17:46:02 +0900

    Rq 클래스 도입으로 로그인 여부와 관련된 중복된 로직 제거

--- src/main/java/com/swc/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/swc/exam/demo/controller/UsrArticleController.java
@@ -12,7 +12,9 @@ import com.swc.exam.demo.service.ArticleService;
 import com.swc.exam.demo.util.Ut;
 import com.swc.exam.demo.vo.Article;
 import com.swc.exam.demo.vo.ResultData;
+import com.swc.exam.demo.vo.Rq;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -24,16 +26,10 @@ public class UsrArticleController {
 	//액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData<Article> doAdd(HttpSession httpSession, String title, String body) {
-		boolean isLogined = false;
-		int loginedMemberId = 0;
+	public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
+		Rq rq = new Rq(req);
 		
-		if ( httpSession.getAttribute("loginedMemberId") != null ) {
-			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
-		}
-
-		if ( isLogined == false ) {
+		if ( rq.isLogined() == false ) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 
@@ -45,25 +41,19 @@ public class UsrArticleController {
 			return ResultData.from("F-2", "body(을)를 입력해주세요.");
 		}
 
-		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(rq.getLoginedMemberId(), title, body);
 		int id = writeArticleRd.getData1();
 
-		Article article = articleService.getForPrintArticles(loginedMemberId, id);
+		Article article = articleService.getForPrintArticles(rq.getLoginedMemberId(), id);
 		
 		return ResultData.newData(writeArticleRd, "article", article);
 	}
 
 	@RequestMapping("/usr/article/list")
-	public String showList(HttpSession httpSession, Model model) {
-		boolean isLogined = false;
-		int loginedMemberId = 0;
+	public String showList(HttpServletRequest req, Model model) {
+		Rq rq = new Rq(req);
 		
-		if ( httpSession.getAttribute("loginedMemberId") != null ) {
-			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
-		}
-		
-		List<Article> articles = articleService.getForPrintArticles(loginedMemberId);
+		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId());
 		
 		model.addAttribute("articles", articles);
 
@@ -72,15 +62,10 @@ public class UsrArticleController {
 	
 	
 	@RequestMapping("/usr/article/detail")
-	public String showDetail(HttpSession httpSession, Model model, int id) {
-		boolean isLogined = false;
-		int loginedMemberId = 0;
+	public String showDetail(HttpServletRequest req, Model model, int id) {
+		Rq rq = new Rq(req);
 		
-		if ( httpSession.getAttribute("loginedMemberId") != null ) {
-			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
-		}
-		Article article = articleService.getForPrintArticles(loginedMemberId, id);
+		Article article = articleService.getForPrintArticles(rq.getLoginedMemberId(), id);
 		
 		model.addAttribute("article", article);
 		
@@ -90,15 +75,10 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public ResultData<Article> getArticle(HttpSession httpSession, int id) {
-		boolean isLogined = false;
-		int loginedMemberId = 0;
+	public ResultData<Article> getArticle(HttpServletRequest req, int id) {
+		Rq rq = new Rq(req);
 		
-		if ( httpSession.getAttribute("loginedMemberId") != null ) {
-			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
-		}
-		Article article = articleService.getForPrintArticles(loginedMemberId, id);
+		Article article = articleService.getForPrintArticles(rq.getLoginedMemberId(), id);
 		
 		if(article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
@@ -111,22 +91,17 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(HttpSession httpSession, int id) {
-		boolean isLogined = false;
-		int loginedMemberId = 0;
+	public String doDelete(HttpServletRequest req, int id) {
+		Rq rq = new Rq(req);
 		
-		if ( httpSession.getAttribute("loginedMemberId") != null ) {
-			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
-		}
 
-		if ( isLogined == false ) {
+		if ( rq.isLogined() == false ) {
 			return Ut.jsHistoryBack("로그인 후 이용해주세요.");
 		}
 
-		Article article = articleService.getForPrintArticles(loginedMemberId, id);
+		Article article = articleService.getForPrintArticles(rq.getLoginedMemberId(), id);
 		
-		if(article.getMemberId() != loginedMemberId) {
+		if(article.getMemberId() != rq.getLoginedMemberId()) {
 			return Ut.jsHistoryBack("권한이 없습니다.");
 		}
 		
@@ -141,29 +116,23 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public ResultData<Article> doModify(HttpSession httpSession, int id, String title, String body) {
-		boolean isLogined = false;
-		int loginedMemberId = 0;
-		
-		if ( httpSession.getAttribute("loginedMemberId") != null ) {
-			isLogined = true;
-			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
-		}
+	public ResultData<Article> doModify(HttpServletRequest req, int id, String title, String body) {
+		Rq rq = new Rq(req);
 		
 
-		if ( isLogined == false ) {
+		if ( rq.isLogined() == false ) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 
 		
-		Article article = articleService.getForPrintArticles(loginedMemberId, id);
+		Article article = articleService.getForPrintArticles(rq.getLoginedMemberId(), id);
 
 		
 		if (article == null) {
 			ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		
-		ResultData actorCanModifyRd = articleService.actorCanModify(loginedMemberId, article);
+		ResultData actorCanModifyRd = articleService.actorCanModify(rq.getLoginedMemberId(), article);
 		
 		if(actorCanModifyRd.isFail()) {
 			return actorCanModifyRd;
