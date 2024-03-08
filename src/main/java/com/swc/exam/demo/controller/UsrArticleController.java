61b78e6dd89af53aec723dec8e5446d5961bbc8f
swc0516s <swc0516@naver.com>
2024-03-08 17:10:57 +0900

    출력용 게시물 가져올 때, 액터 정보 넘겨서 더욱 자세한 정보 생성하기

--- src/main/java/com/swc/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/swc/exam/demo/controller/UsrArticleController.java
@@ -48,14 +48,22 @@ public class UsrArticleController {
 		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
 		int id = writeArticleRd.getData1();
 
-		Article article = articleService.getForPrintArticles(id);
+		Article article = articleService.getForPrintArticles(loginedMemberId, id);
 		
 		return ResultData.newData(writeArticleRd, "article", article);
 	}
 
 	@RequestMapping("/usr/article/list")
-	public String showList(Model model) {
-		List<Article> articles = articleService.getForPrintArticles();
+	public String showList(HttpSession httpSession, Model model) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		
+		if ( httpSession.getAttribute("loginedMemberId") != null ) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		
+		List<Article> articles = articleService.getForPrintArticles(loginedMemberId);
 		
 		model.addAttribute("articles", articles);
 
@@ -64,8 +72,15 @@ public class UsrArticleController {
 	
 	
 	@RequestMapping("/usr/article/detail")
-	public String showDetail(Model model, int id) {
-		Article article = articleService.getForPrintArticles(id);
+	public String showDetail(HttpSession httpSession, Model model, int id) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		
+		if ( httpSession.getAttribute("loginedMemberId") != null ) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		Article article = articleService.getForPrintArticles(loginedMemberId, id);
 		
 		model.addAttribute("article", article);
 		
@@ -75,8 +90,15 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public ResultData<Article> getArticle(int id) {
-		Article article = articleService.getForPrintArticles(id);
+	public ResultData<Article> getArticle(HttpSession httpSession, int id) {
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		
+		if ( httpSession.getAttribute("loginedMemberId") != null ) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		Article article = articleService.getForPrintArticles(loginedMemberId, id);
 		
 		if(article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
@@ -102,7 +124,7 @@ public class UsrArticleController {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
 		}
 
-		Article article = articleService.getForPrintArticles(id);
+		Article article = articleService.getForPrintArticles(loginedMemberId, id);
 		
 		if(article.getMemberId() != loginedMemberId) {
 			return ResultData.from("F-2", "권한이 없습니다.");
@@ -134,7 +156,7 @@ public class UsrArticleController {
 		}
 
 		
-		Article article = articleService.getForPrintArticles(id);
+		Article article = articleService.getForPrintArticles(loginedMemberId, id);
 
 		
 		if (article == null) {
