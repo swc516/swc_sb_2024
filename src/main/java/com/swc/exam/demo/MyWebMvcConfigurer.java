553fb19db2f80cd5d36a4f0161d7fccf3e464472
swc0516s <swc0516@naver.com>
2024-03-09 12:09:15 +0900

    BeforeAction 인터셉터 등록

---
+++ src/main/java/com/swc/exam/demo/MyWebMvcConfigurer.java
@@ -0,0 +1,23 @@
+package com.swc.exam.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.swc.exam.demo.interceptor.BeforeActionInterceptor;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer{
+	// beforeActionInterceptor 인터셉터 불러오기
+	@Autowired
+	BeforeActionInterceptor beforeActionInterceptor;
+
+	// 이 함수는 인터셉터를 적용하는 역할을 합니다.
+	// resource/common.css 이런거는 제외
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/error");
+	}
+}
