d4622ad4583fb6f7ea7baffb388da0821f9aedac
swc0516s <swc0516@naver.com>
2024-03-11 16:22:30 +0900

    BeforeActionInterceptor에서 Rq객체를 좀 더 확실하게 생성되도록 유도

--- src/main/java/com/swc/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/swc/exam/demo/controller/UsrHomeController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.swc.exam.demo.vo.Rq;
+
 
 @Controller
 public class UsrHomeController {
-
 	@RequestMapping("/usr/home/main")
 	public String showMain() {
 		return "usr/home/main";
--- src/main/java/com/swc/exam/demo/vo/Rq.java
+++ src/main/java/com/swc/exam/demo/vo/Rq.java
@@ -98,4 +98,11 @@ public class Rq {
 		return Ut.jsReplace(msg, uri);
 	}
 
+	// 이 메서드는 Rq 객체가 자연스럽게 생성되도록 유도하는 역할을 한다.
+	// 지우면 안되고,
+	// 편의를 위해 initOnBeforeActionInterceptor에서 꼭 호출을 해야 한다.
+	public void initOnBeforeActionInterceptor() {
+		
+	}
+
 }
