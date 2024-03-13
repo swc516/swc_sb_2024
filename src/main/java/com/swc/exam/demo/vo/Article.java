8544e607f94941a364f91ce83ac0c2c2413228c7
swc0516s <swc0516@naver.com>
2024-03-13 15:45:25 +0900

    시간출력관련 함수를 Article 클래스에 만들어서 JSP에서 사용

--- src/main/java/com/swc/exam/demo/vo/Article.java
+++ src/main/java/com/swc/exam/demo/vo/Article.java
@@ -19,12 +19,20 @@ public class Article {
 	private String extra__writerName;
 	private boolean extra__actorCanDelete;
 	private boolean extra__actorCanModify;
-	
-	public String getRegDateForPrint() {
-		return regDate.substring(2, 16);
+
+	public String getForPrintType1RegDate() {
+		return regDate.substring(2,16).replace(" ", "<br>");
 	}
 	
-	public String getUpdateDateForPrint() {
+	public String getForPrintType1UpdateDate() {
+		return updateDate.substring(2,16).replace(" ", "<br>");
+	}
+
+	public String getForPrintType2RegDate() {
+		return regDate.substring(2, 16);
+	}
+
+	public String getForPrintType2UpdateDate() {
 		return updateDate.substring(2, 16);
 	}
 }
