f98381a2cdace3ff25b822ff79780634b4d26e80
swc0516s <swc0516@naver.com>
2024-03-16 20:39:32 +0900

    마이페이지 폼 생성

--- src/main/java/com/swc/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/swc/exam/demo/controller/UsrMemberController.java
@@ -113,5 +113,11 @@ public class UsrMemberController {
 
 		return rq.jsReplace("로그아웃 되었습니다.", "/");
 	}
+	
+	@RequestMapping("/usr/member/myPage")
+	public String showMyPage() {
+		return "usr/member/myPage";
+	}
+
 
 }
