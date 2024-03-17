5a84f24ead58228d1a1d56f02765d605dbd0566f
swc0516s <swc0516@naver.com>
2024-03-17 18:56:07 +0900

    로그인 후, 원래 가려던 페이지로 이동시켜주는 기능 구현

--- src/main/java/com/swc/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/swc/exam/demo/controller/UsrMemberController.java
@@ -3,6 +3,7 @@ package com.swc.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.swc.exam.demo.service.MemberService;
@@ -68,7 +69,7 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
-	public String doLogin(String loginId, String loginPw) {
+	public String doLogin(String loginId, String loginPw, @RequestParam(defaultValue="/")String afterLoginUri) {
 		if (rq.isLogined()) {
 			return rq.jsHistoryBack("이미 로그인되었습니다.");
 		}
@@ -93,7 +94,7 @@ public class UsrMemberController {
 
 		rq.login(member);
 
-		return rq.jsReplace(Ut.f("%s님 환영합니다", member.getNickname()), "/");
+		return rq.jsReplace(Ut.f("%s님 환영합니다", member.getNickname()), afterLoginUri);
 	}
 
 	@RequestMapping("/usr/member/login")
--- src/main/webapp/WEB-INF/jsp/usr/member/login.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/member/login.jsp
@@ -7,6 +7,7 @@
 <section class="mt-5">
   <div class="container mx-auto px-3">
     <form class="table-box-type-1" method="post" action="../member/doLogin">
+    <input type="hidden" name="afterLoginUri" value="${param.afterLoginUri}"/>
       <table border="1">
         <tbody>
         <colgroup>
